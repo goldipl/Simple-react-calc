014f18a2e4507bc5e976a32928ec50e051b8295e
goldipl <mgodlewski92@gmail.com>
2022-03-15 08:21:26 +0100

    handle calc options

--- src/comp/Calc.js
+++ src/comp/Calc.js
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const InitialValue = 100;
+const InitialValue = '';
 
 export default function App() {
-  const [counter, setCounter] = React.useState(InitialValue);
-  const [step, setStep] = React.useState(10);
+  const [value1, setValue1] = useState(InitialValue);
+  const [value2, setValue2] = useState(InitialValue);
+  const [result, setResult] = useState(InitialValue);
+
+  const add = () => {
+    setResult(value1 + value2);
+  };
+
+  const multiply = () => {
+    setResult(value1 * value2);
+  };
+
+  const divide = () => {
+    if (value2 !== 0) {
+      setResult(value1 / value2);
+    } else {
+        alert('Nie można dzielić przez zero!');
+    }
+  };
+
+  const substract = () => {
+    setResult(value1 - value2);
+  };
 
   return (
     <div>
-      <h2>Licznik: {counter}</h2>
-      <input value={step} onChange={(event) => setStep(Number(event.target.value))} />
-      <div>
-        <button onClick={() => setCounter(counter + step)}>Increment</button>
-        <button onClick={() => setCounter(InitialValue)}>Reset</button>
+      <h2>Calculator - ver. 0.1</h2>
+      <div className='inputContainer'>
+      <input placeholder='Type 1st number' value={value1} onChange={(e) => setValue1(Number(e.target.value))} />
+      <input placeholder='Type 2nd number' value={value2} onChange={(e) => setValue2(Number(e.target.value))} />
+      </div>
+      <div className='btnContainer'>
+        <button onClick={add}>Add</button>
+        <button onClick={multiply}>Multiply</button>
+        <button onClick={divide}>Divide</button>
+        <button onClick={substract}>Substract</button>
+      </div>
+      <div className='inputContainer'>
+      <input placeholder='result' readOnly value={result}  />
       </div>
     </div>
   );
